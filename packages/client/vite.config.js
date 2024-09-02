f31e8c92be96e9377d1c23a648ea4e4213cb741f
gineff <canone@inbox.ru>
2024-09-02 12:32:02 +0300

    chore: add directus proxy

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable no-undef */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import alias from '@rollup/plugin-alias';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    alias({
+      entries: [
+        { find: '@', replacement: path.resolve(__dirname, './src') },
+        { find: '@entities', replacement: path.resolve(__dirname, './src/entities') },
+        { find: '@widgets', replacement: path.resolve(__dirname, './src/widgets') },
+      ],
+    }),
+  ],
+  server: {
+    proxy: {
+      '/directus': {
+        target: 'http://localhost:8055',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/directus/, ''),
+      },
+      '/admin': {
+        target: 'http://localhost:8055',
+        changeOrigin: true,
+        followRedirects: true,
+      },
+    },
+  },
+});
